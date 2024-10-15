b13515e21d4ba6abc889e6a7a79502de17856c7a
Filipe Mendes <fmatiasmendes@gmail.com>
2024-10-15 15:15:34 +0100

    Lesson 11 - Part II

--- Lesson_11_Arrays_and_Loops/exercises/11x.js
+++ Lesson_11_Arrays_and_Loops/exercises/11x.js
@@ -1,67 +1,4 @@
-const todoList = [];
-const todoList2 = [];
-
-
-renderTodoList();
-
-function addTodo(){
-	const inputElement = document.querySelector('.js-name-input');
-
-	const name = inputElement.value;
-	//console.log(name);
-
-	todoList.push(name);
-	console.log(todoList);
-
-	//to reset the text in the input text box
-	inputElement.value = '';
-
-}
-
-function renderTodoList(){
-
-	let todoListHTML = '';
-
-	for (let i = 0; i < todoList2.length; i++){
-
-		const todo = todoList2 [i];
-		const html = `<P>${todo}</p>`;
-		todoListHTML += html;
-
-	}
-
-	document.querySelector('.js-todo-list2').innerHTML = todoListHTML;
-	console.log(todoListHTML);
-}
-
-
-function addTodo2(){
-	const inputElement2 = document.querySelector('.js-name-input-2');
-
-	const name2 = inputElement2.value;
-	//console.log(name2);
-
-	todoList2.push(name2);
-	console.log(todoList2);
-
-	//to reset the text in the input text box
-	inputElement2.value = '';
-
-	renderTodoList();
-}
-
-
-
-
-///////JS for last part of the exercise/////////
-
-const todoList3 = [{
-  name: 'make dinner',
-  dueDate: '2022-12-22'
-}, {
-  name: 'wash dishes',
-  dueDate: '22024-01-05'
-}];
+const todoList3 = JSON.parse(localStorage.getItem('savedTodoList')) || [];
 
 renderTodoList3();
 
@@ -113,4 +50,9 @@ function renderTodoList3(){
 
 	document.querySelector('.js-todo-list3').innerHTML = todoListHTML;
 	console.log(todoListHTML);
+
+	const storageTodoList3 = JSON.stringify(todoList3);
+
+	localStorage.setItem('savedTodoList', storageTodoList3);
+
 }
