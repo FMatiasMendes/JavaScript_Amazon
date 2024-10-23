e7fd271c8acdede27a3d38b114aad334dce83926
Filipe Mendes <fmatiasmendes@gmail.com>
2024-10-23 12:41:03 +0100

    Create automated tests for formatCurrency

---
+++ javascript-amazon-project/tests/moneyTest.js
@@ -0,0 +1,44 @@
+//to import from money.js
+import {formatCurrency} from '../scripts/utils/money.js';
+
+console.log('---test suite: formatCurrency---');
+
+//test case
+console.log('converts cents into dollars');
+
+if (formatCurrency(2095) === '20.95'){
+    console.log('passed');
+}
+else{
+    console.log('FAILED!!!');
+}
+
+//edge case
+console.log('works with 0');
+
+if (formatCurrency(0) === '0.00'){
+	console.log('passed');
+}
+else{
+    console.log('FAILED!!');
+}
+
+//edge case
+console.log('rounds up to the nearest cent');
+
+if (formatCurrency(2000.5) === '20.01'){
+	console.log('passed');
+}
+else{
+    console.log('FAILED!!!');
+}
+
+//edge case
+console.log('rounds down to the nearest cent');
+
+if (formatCurrency(2000.4) === '20.00'){
+	console.log('passed');
+}
+else{
+    console.log('FAILED!!!');
+}
