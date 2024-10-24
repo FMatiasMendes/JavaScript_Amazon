d036d1482206559ba097a6bac1f024fef59b9651
Filipe Mendes <fmatiasmendes@gmail.com>
2024-10-24 10:40:41 +0100

    Create oop version of cart

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -2,6 +2,8 @@
 import {renderOrderSummary} from './checkout/orderSummary.js'
 //to import from paymentSummary.js
 import {renderPaymentSummary} from './checkout/paymentSummary.js'
+//just to run and test the cart-oop file without importing anything (same thing as cart.js but modified to object-oriented programming)
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 renderPaymentSummary();
