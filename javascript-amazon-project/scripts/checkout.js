cb099d192222a2cdc026981565d43645576d8650
Filipe Mendes <fmatiasmendes@gmail.com>
2024-10-24 12:33:56 +0100

    Convert products into classes

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -5,7 +5,7 @@ import {renderPaymentSummary} from './checkout/paymentSummary.js'
 //just to run and test the cart-oop file without importing anything (same thing as cart.js but modified to object-oriented programming)
 //import '../data/cart-oop.js';
 //just to run and test the cart-class file without importing anything (same thing as cart.js but modified to object-oriented programming)
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
